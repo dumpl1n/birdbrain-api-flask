9411c85680e67e0a96aaf958c2ec183f20f02277
dumpl1n <kiccassidy@gmail.com>
2023-12-09 23:28:44 -0500

    database setup

---
+++ migrations/database-setup.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS birds (
+    id serial PRIMARY KEY,
+    class_id INT(255) NOT NULL,
+    file_path VARCHAR(255) NOT NULL,
+    labels VARCHAR(255) NOT NULL,
+    data_set VARCHAR(255) NOT NULL,
+    scientific_name VARCHAR(255) NOT NULL,
+);
