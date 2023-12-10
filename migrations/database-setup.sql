79626231ec095f4a0e1ded68e09352024815cbe3
dumpl1n <kiccassidy@gmail.com>
2023-12-10 13:17:36 -0500

    created birds_db  database

--- migrations/database-setup.sql
+++ migrations/database-setup.sql
@@ -1,8 +1,9 @@
 CREATE TABLE IF NOT EXISTS birds (
     id serial PRIMARY KEY,
-    class_id INT(255) NOT NULL,
+    class_id INT NOT NULL,
     file_path VARCHAR(255) NOT NULL,
     labels VARCHAR(255) NOT NULL,
     data_set VARCHAR(255) NOT NULL,
     scientific_name VARCHAR(255) NOT NULL
 );
+-- \copy table_name (col_1, col_2) FROM '/path/to/data.csv' DELIMITER ',' CSV HEADER;
