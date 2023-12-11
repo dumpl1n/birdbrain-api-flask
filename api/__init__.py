efb31c7b87d37140ee1f2e50b8699bd0cc52bdd1
dumpl1n <kiccassidy@gmail.com>
2023-12-10 19:24:48 -0500

    connected show_bird route. bird images now display.

--- api/__init__.py
+++ api/__init__.py
@@ -1,11 +1,11 @@
 # import dependencies
 import os
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, render_template
 import psycopg2
 from bird import Bird
 
-app = Flask(__name__)
-
+app = Flask(__name__, static_folder='../data/bird-training-images/', template_folder='../templates/')
+# app = Flask(__name__, static_folder='data/', template_folder=os.path.join(basedir, 'templates'))
 #use the `app.config.from_mapping` method to set these variables on our flask application
 app.config.from_mapping(
     DATABASE = os.getenv('DATABASE'),
@@ -29,9 +29,18 @@ def birds():
 def show_bird(id):
     conn = psycopg2.connect(database=app.config['DATABASE'], user=app.config['USER'])
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM venues WHERE id = %s LIMIT 1;", id)
-    bird_inst = cursor.fetchone()
-    return jsonify(Bird(bird_inst).__dict__)
+    cursor.execute("SELECT * FROM birds WHERE id = %s LIMIT 1;", (id,))
+    bird_data = cursor.fetchone()
+    if bird_data:
+        bird = Bird(bird_data)
+        # bird_dict = bird.__dict__
+        # bird_dict['image_url'] = bird.image_url()
+        bird_image = bird.image_url()
+        return render_template('bird_image.html', image_url=bird_image) # jsonify(bird_dict)
+    else:
+        return jsonify({"error": "Bird not found"}), 404
+    # return jsonify(Bird(bird_data).__dict__)
+
 
 app.run(debug = True)
 
--- api/bird.py
+++ api/bird.py
@@ -1,7 +1,15 @@
+from flask import url_for
 class Bird:
     __tablename__ = 'birds'
-    columns = ['id','class_id','filepaths','labels','data_set','scientific_name']
+    columns = ['id','class_id','file_path','labels','data_set','scientific_name']
     
     def __init__(self, values):
         self.__dict__ = dict(zip(self.columns, values))
-    
+
+    # display bird image located in data/bird-training-images
+    def image_url(self):
+        # url_root = '/Users/kielay/code/projects/flask-bird-api'
+        # return url_for( '/data/bird-training-images/' + self.file_path, _external=True)
+        return url_for('static',filename=self.file_path)
+        
+        # return request.url_root + '/Users/kielay/code/projects/flask-bird-api/data/bird-training-images/' + self.filepaths
